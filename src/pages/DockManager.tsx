d9622399d53359788f6cf82845a12795e2f61ad6
avanish-srivastava <avanish.srivastava@nirvanasolutions.com>
2023-11-17 13:24:34 +0530

    Cleanup of dock manager component

--- src/pages/DockManager.tsx
+++ src/pages/DockManager.tsx
@@ -9,7 +9,6 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       'igc-dockmanager': any;
-      'igc-button-component': any;
     }
   }
 }
@@ -71,25 +70,10 @@ const DockManagerOverview = () => {
   };
 
   useEffect(() => {
-    // const pane1 = createContentPane('content1', 'Content Pane 1');
     const pane1 = createContentPane('content1', 'Unpinned Pane 1');
     pane1.isPinned = false;
 
-    // const pane3 = createContentPane('content3', 'Document 1');
-    // const pane4 = createContentPane('content4', 'Document 2');
-
-    // const contentPane5 = createContentPane('content5', 'Unpinned Pane 2');
-    // contentPane5.isPinned = false;
-
-    // const pane6 = createContentPane('content6', 'Tab 1');
-    // const pane7 = createContentPane('content7', 'Tab 2');
-    // const pane8 = createContentPane('content8', 'Content Pane 2');
-    // const pane9 = createContentPane('content9', 'Floating Pane');
-
-    // const tabPane1 = createTabPane(IgcSplitPaneOrientation.horizontal, [pane3], 200);
-
     const splitPane1 = createSplitPane(IgcSplitPaneOrientation.vertical, [pane1]);
-    // const splitPane2 = createSplitPane(IgcSplitPaneOrientation.vertical, [tabPane1, contentPane5], 200);
 
     dockManagerRef.current = document.getElementById('dockManager') as IgcDockManagerComponent;
     dockManagerRef.current.layout = {
@@ -98,31 +82,8 @@ const DockManagerOverview = () => {
         orientation: IgcSplitPaneOrientation.horizontal,
         panes: [
           splitPane1,
-          // splitPane2,
-          // {
-          //   type: IgcDockManagerPaneType.splitPane,
-          //   orientation: IgcSplitPaneOrientation.vertical,
-          //   panes: [
-          //     {
-          //       type: IgcDockManagerPaneType.tabGroupPane,
-          //       size: 200,
-          //       panes: [pane6],
-          //     },
-          //     pane8,
-          //   ],
-          // },
         ],
       },
-      // floatingPanes: [
-      //   {
-      //     type: IgcDockManagerPaneType.splitPane,
-      //     orientation: IgcSplitPaneOrientation.horizontal,
-      //     floatingHeight: 400,
-      //     floatingWidth: 850,
-      //     floatingLocation: { x: 300, y: 300 },
-      //     panes: [pane9],
-      //   },
-      // ],
     };
   }, []);
 
@@ -184,9 +145,6 @@ const DockManagerOverview = () => {
   return (
     <div>
       <igc-dockmanager id="dockManager" ref={dockManagerRef}>
-        {/* <div slot="content1" className="dockManagerContent">
-          <SampleGrid />
-        </div> */}
         <div slot="content1" className="dockManagerContent">
           <button onClick={handleButtonClick}>Add new pane</button>
           <button onClick={saveLayout} className='saveButton'>Save Layout</button>
@@ -196,54 +154,6 @@ const DockManagerOverview = () => {
             {contentName === 'grid' ? <SampleGrid /> : <DataGrid />}
           </div>
         ))}
-
-        {/* <button slot="closeButton">x</button>
-
-        <button slot="maximizeButton">
-          <img src="https://www.svgrepo.com/show/419558/arrow-top-chevron-chevron-top.svg" alt="" />
-        </button>
-
-        <button slot="minimizeButton">
-          <img src="https://www.svgrepo.com/show/419557/bottom-chevron-chevron-down.svg" alt="" />
-        </button>
-
-        <button slot="pinButton">
-          <img src="https://www.svgrepo.com/show/154123/pin.svg" alt="" />
-        </button>
-
-        <button slot="unpinButton">
-          <img src="https://www.svgrepo.com/show/154123/pin.svg" alt="" />
-        </button> */}
-        {/* {contentName === 'blotter' ? (
-          <div slot={`${paneName}`} className="dockManagerContent">
-            <Blotter />
-          </div>
-        ) : contentName === 'tt' ? (
-          <div slot={`${paneName}`} className="dockManagerContent">
-            <TradingTicket />
-          </div>
-        ) : null} */}
-        {/* <div slot={`${paneName}`} className="dockManagerContent">
-          {contentName === 'blotter' ? <Blotter /> : <TradingTicket />}
-        </div> */}
-        {/* <div slot="content4" className="dockManagerContent">
-          <SampleGrid />
-        </div>
-        <div slot="content5" className="dockManagerContent">
-          <SampleGrid />
-        </div>
-        <div slot="content6" className="dockManagerContent">
-          <SampleGrid />
-        </div>
-        <div slot="content7" className="dockManagerContent">
-          <SampleGrid />
-        </div>
-        <div slot="content8" className="dockManagerContent">
-          <Blotter />
-        </div>
-        <div slot="content9" className="dockManagerContent">
-          <TradingTicket />
-        </div> */}
       </igc-dockmanager>
     </div>
   );
